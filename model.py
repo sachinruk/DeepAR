a523560616039aa2ab39b5117aa529f71f78f8ef
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2019-05-31 17:31:33 +1000

    Have a mostly complete probabilisitic LSTM for regression tasks

--- model.py
+++ model.py
@@ -7,20 +7,13 @@ class ProbabilisticLSTM(nn.Module):
         super().__init__()
         self.lstm = nn.LSTM(n_features, h, num_layers)
         self.linear = nn.Linear(h, n_features*2)
-        # self.h = h
-        # self.n = n_features
 
-    # def init_hidden(self):
-    #     # This is what we'll initialise our hidden state as
-    #     return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
-    #             torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
-
-    def forward(self, input):
+    def forward(self, input, h=None):
         # Forward pass through LSTM layer
         # shape of lstm_out: [input_size, batch_size, hidden_dim]
         # shape of self.hidden: (a, b), where a and b both 
         # have shape (num_layers, batch_size, hidden_dim).
-        lstm_out, self.hidden = self.lstm(input)
+        lstm_out, self.hidden = self.lstm(input, h)
         
         # Only take the output from the final timetep
         # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
@@ -32,9 +25,29 @@ class ProbabilisticLSTM(nn.Module):
         sigma = torch.log(1 + torch.exp(sigma)) # make numbers positive
         return mu, sigma, self.hidden
 
-    def sample(self, input, n_samples, t_ahead):
-        # pass
-        # # for 
-        mu, sigma, h = self.forward(input)
-        
+    def sample(self, input, h, n_samples, t_ahead):
+        """
+        Returns one sample trajectory 
+        """
+        shape = input.shape
+        y_sample = torch.zeros((t_ahead,)+shape[1:])
+        for t in range(t_ahead):
+            mu, sigma, h = self.forward(input, h)
+            y_sample[t] = mu + sigma * torch.randn(shape)
+            input = y_sample[t]
 
+        return y_sample
+
+
+class LSTM_(nn.Module):
+    def __init__(self, f,h,n):
+        super().__init__()
+        self.lstm = nn.LSTM(f,h,n)
+        self.linear = nn.Linear(h, f)
+        self.h = None
+
+    def forward(self, x, h=None):
+        out, self.h = self.lstm(x, h)
+        y = self.linear(out)
+
+        return y, self.h
