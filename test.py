b71e18a25fdbbd17b844c08645c111f2eb810cdd
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2019-06-04 14:06:39 +1000

    Testing on sine waves over multiple epochs

--- test.py
+++ test.py
@@ -1,29 +1,63 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch import optim
+
+import numpy as np
+from util import create_X
+from model import LSTM_
+
 import matplotlib.pyplot as plt
 
-from util import get_rnn_batch
-
-from model import ProbabilisticLSTM
-
-x = torch.arange(0,30,0.05)
+# x = torch.arange(1000)[:,None].repeat(1,3).float()
+x = torch.arange(0,30,0.01)
 y = [torch.sin(x), torch.sin(x-np.pi), torch.sin(x-np.pi/2)]
 y = torch.stack(y)
 y = y.t()
+x = y
 
-for y_ in y:
-    plt.plot(x.cpu().numpy(), y_.cpu().numpy())
+print('Shape', x.shape)
+
+
+model = LSTM_(3, 5, 2)
+opt = optim.Adam(model.parameters())
+loss_func = nn.MSELoss()
+losses = []
+epochs = 30
+t_back = 20
+
+for epoch in range(epochs):
+    batches = create_X(x, t_back, 5)
+    for i, (batchX, batchY) in enumerate(batches):
+        # print(i, batchX[0].shape, batchY[0].shape)
+        o, h = model(batchX)
+        # import pdb; pdb.set_trace()
+        loss = loss_func(o, batchY[0])
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        losses.append(loss)
+    model.init_hidden()
+
+plt.plot(losses)
 plt.show()
 
-train_len = int(len(x) * 0.8)
-train_y, test_y = y[:train_len], y[train_len:]
+# x = torch.arange(0,30,0.01)
+# y = [torch.sin(x), torch.sin(x-np.pi), torch.sin(x-np.pi/2)]
+# y = torch.stack(y)
+# y = y.t()
+# x = y
 
-bs = 30
-batches = get_rnn_batch(train_y, bs)
+# for y_ in y:
+#     plt.plot(x.cpu().numpy(), y_.cpu().numpy())
+# plt.show()
 
-for x,y in batches:
+# train_len = int(len(x) * 0.8)
+# train_y, test_y = y[:train_len], y[train_len:]
+
+# bs = 30
+# batches = get_rnn_batch(train_y, bs)
+
+# for x,y in batches:
     
 
 
